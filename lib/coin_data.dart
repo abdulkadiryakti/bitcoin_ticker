671c416ec4f10238690650df5a047dc2032e3e8c
Abdulkadir Yaktı <abdulkadiryakti49@gmail.com>
2023-10-12 00:52:59 +0300

    first commit

---
+++ lib/coin_data.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'TRY',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = ['BTC', 'ETH', 'LTC'];
+
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = '735F34CC-68EC-46DC-B04A-CE8015B40F43';
+
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
+
+    for (String crypto in cryptoList) {
+      String requestURL =
+          '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
+      http.Response response = await http.get(Uri.parse(requestURL));
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double rate = decodedData['rate'];
+        cryptoPrices[crypto] = rate.toStringAsFixed(2);
+      } else {
+        print(response.statusCode);
+        throw ('There is a problem getting data.');
+      }
+    }
+    return cryptoPrices;
+  }
+}
